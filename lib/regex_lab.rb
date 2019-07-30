58240e3456b825a2231ec51d492c744eaf087f41
Raul Sposito <raulsposito@gmail.com>
2019-07-30 14:12:22 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -25,7 +25,7 @@ return new_text_array
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z]/) && text.match(/[ _.,!"']$/) ? true : false
 end
 
 def valid_phone_number?(phone)
