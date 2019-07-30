cbe1c3ee13c90fb679b174090db549441d484530
Raul Sposito <raulsposito@gmail.com>
2019-07-30 14:08:41 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,16 @@
 def starts_with_a_vowel?(word)
-
+  word.match(/\A[aeiouAEIOU]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  new_text_array = []
+  original_string = text.split(" ")
+  original_string.each do |word|
+    if word.match(/(^un)|(ing$)/)
+      new_text_array.push(word)
+    end
+  end
+  return new_text_array
 end
 
 def words_five_letters_long(text)
